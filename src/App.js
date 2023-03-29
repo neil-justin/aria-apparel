08110bfc15b79800e0b35eeacb5ec5af04a7ebe5
neil <mallarineil9@gmail.com>
2023-03-29 04:42:15 -0400

    Setup the route for the Home navigation link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './styles/header.css';
 import './styles/index.css';
+import './styles/home.css';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header>
         <span id="logo">Aria Apparel</span>
+
+        <nav id='header-nav'>
+          <ul>
+            <li><NavLink to='/'>Home</NavLink></li>
+          </ul>
+        </nav>
       </header>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <main id="home-main-content">
+            <div id="hero">
+                <h1>Welcome to Aria Apparel!</h1>
+                <p id="hero-para">
+                    Shop with ease and convenience at Aria Apparel. Find
+                    unbeatable prices on high-quality products, all in
+                    one convenient location
+                </p>
+                <button id='hero-button'>Shop now</button>
+            </div>
+        </main>
+    )
+}
