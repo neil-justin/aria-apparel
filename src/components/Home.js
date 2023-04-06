cb35e5b001f6a734def6276ff1388b370362d98a
neil <mallarineil9@gmail.com>
2023-04-06 02:25:29 -0400

    Improve mobile looks of hero

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main id="home-main-content">
             <div id="hero">
-                <h1>Welcome to Aria Apparel!</h1>
+                <h1 id="hero-heading">Welcome to Aria Apparel!</h1>
                 <p id="hero-para">
                     Shop with ease and convenience at Aria Apparel. Find
                     unbeatable prices on high-quality products, all in
